abedb021884b986be7a8aad394fce5453c6ccfb0
Patrick <pat-sie@web.de>
2018-05-27 22:16:49 +0200

    added code for factory pattern

---
+++ documentation/patterns/EnemyFactory.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFactory {
+
+	GameObject enemy;
+	
+	public static EnemyFactory create()
+	{
+		return new EnemyFactory();
+	}
+
+	public EnemyFactory()
+	{
+		this.enemy = new GameObject();
+		enemy.AddComponent<EnemyScript>();
+	}
+
+	public EnemyFactory withSprite(string pathToSprite)
+	{
+		enemy.AddComponent<SpriteRenderer>();
+		enemy.GetComponent<SpriteRenderer>().sprite = Resources.Load(pathToSprite) as Sprite;
+		return this;
+	}
+
+	public EnemyFactory withSpeed(float speed)
+	{
+		enemy.GetComponent<EnemyScript>().speed = speed;
+		return this;
+	}
+
+	public EnemyFactory withWeapon(AbstractWeapon weapon)
+	{
+		enemy.GetComponent<EnemyScript>().weaponOnHand = weapon;
+		return this;
+	}
+
+	public EnemyFactory atPosition(Vector2 position)
+	{
+		enemy.transform.position = position;
+		return this;
+	}
+
+	public GameObject get()
+	{
+		return enemy;
+	}
+}
---
+++ documentation/patterns/TileFactory.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Generation
+{
+	public class TileFactory : MonoBehaviour
+	{
+		public static GameObject LoadGroundTile()
+		{
+			return GameObjectCache.Load("Dungeon/GroundTile");
+		}
+
+		public static GameObject LoadWallTile()
+		{
+			return GameObjectCache.Load("Dungeon/WallTile");
+		}
+
+		public static GameObject LoadDoorTile()
+		{
+			return GameObjectCache.Load("Dungeon/DoorTile");
+		}
+
+		public static GameObject LoadLockedDoorTile()
+		{
+			return GameObjectCache.Load("Dungeon/LockedDoorTile");
+		}
+
+		public static GameObject LoadGateTile()
+		{
+			return GameObjectCache.Load("Dungeon/GateTile");
+		}
+
+		public static GameObject LoadLavaTile()
+		{
+			return GameObjectCache.Load("Terrain/LavaTile");
+		}
+
+		public static GameObject LoadSandTile()
+		{
+			return GameObjectCache.Load("Terrain/SandTile");
+		}
+
+		public static GameObject LoadAcceleratorTile()
+		{
+			return GameObjectCache.Load("Terrain/AcceleratorTile");
+		}
+
+		public static GameObject LoadBarrel()
+		{
+			return GameObjectCache.Load("Obstacles/Barrel");
+		}
+
+		public static Tile CreateGroundTile(Coordinate coordinate, Column x, Row y, int coordinateTileCount)
+		{
+			return new Tile(coordinate, x, y, coordinateTileCount, LoadGroundTile(), new LockState(false));
+		}
+
+		public static Tile CreateWallTile(Coordinate coordinate, Column x, Row y, int coordinateTileCount)
+		{
+			return new Tile(coordinate, x, y, coordinateTileCount, LoadWallTile(), new LockState(false));
+		}
+
+		public static Tile CreateDoorTile(Coordinate coordinate, Column x, Row y, int coordinateTileCount,
+			LockState lockState)
+		{
+			Tile doorTile = new Tile(coordinate, x, y, coordinateTileCount, LoadDoorTile(), lockState);
+			return doorTile;
+		}
+
+		public static Tile CreateBarrel(Coordinate coordinate, Column x, Row y, int coordinateTileCount)
+		{
+			return new Tile(coordinate, x, y, coordinateTileCount, LoadBarrel(), new LockState(false));
+		}
+	}
+}
